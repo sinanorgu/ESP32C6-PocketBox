952b1fdaa933eeb6744d0e3179940c745d87fd92
sinanorgu <sinanorguuu@gmail.com>
2026-09-14 20:56:43 +0300

    optimize drawing listMenu

--- include/ListMenu.hpp
+++ include/ListMenu.hpp
@@ -16,14 +16,16 @@ public:
 class ListMenu {
 
     public:
-        ListMenu() : innerIndex(0), offset(0), changed(true), itemCount(0), maxVisibleItems(0), textSize(2), textHeight(0), headerHeight(0), header("") {};
+        ListMenu() : innerIndex(0), offset(0), changed(true), itemCount(0), maxVisibleItems(0), textSize(2), textHeight(0), headerHeight(0), header(""), fullRedraw(true), renderedInnerIndex(0), renderedOffset(0), hasRendered(false) {};
         void setHeader(const String& header){
             this->header = header;
-            headerHeight = header.length() > 0 ? textHeight : 0;
+            updateLayout();
+            invalidate();
+        }
+        void invalidate(){
             changed = true;
-            if(maxVisibleItems > 0){
-                maxVisibleItems--;
-            }
+            fullRedraw = true;
+            hasRendered = false;
         }
         void draw() {
             if(System::getInstance().gfx == nullptr || changed == false){
@@ -31,28 +33,30 @@ class ListMenu {
                 return;
             }
             Serial.printf("reached line:%d in file %s\n", __LINE__, __FILE__);
-            System::getInstance().gfx->fillRect(x, y, width, height, COLOR_BACKGROUND);
             System::getInstance().gfx->setTextSize(textSize);
-            if(header.length() > 0){
+            if(fullRedraw){
+                clearItems();
+            }
+            if(fullRedraw && header.length() > 0){
                 System::getInstance().gfx->fillRect(x + 2, y + 2, width - 4, textHeight, RGB565_DARKGREY);
                 System::getInstance().gfx->setCursor(x + 5, y + 5);
                 System::getInstance().gfx->setTextColor(RGB565_YELLOW);
                 System::getInstance().gfx->print(header);
             }
 
-            for(int i = 0; i < maxVisibleItems && i < itemCount; i++){
-                int itemIndex = offset + i;
-                if(itemIndex < 64 && items[itemIndex]->name.length() > 0){
-                    if(i == innerIndex){
-                        System::getInstance().gfx->fillRect(x + 2, y + 2 + headerHeight + i * textHeight, width - 4, textHeight, RGB565_CYAN);
-                        System::getInstance().gfx->setTextColor(RGB565_BLACK);
-                    } else {
-                        System::getInstance().gfx->setTextColor(RGB565_WHITE);
-                    }
-                    System::getInstance().gfx->setCursor(x + 5, y + 5 + headerHeight + i * textHeight);
-                    System::getInstance().gfx->print(items[itemIndex]->name);
+            if(fullRedraw || !hasRendered){
+                for(int i = 0; i < maxVisibleItems && i < itemCount; i++){
+                    drawItem(i, offset + i, i == innerIndex);
                 }
+            } else {
+                drawItem(renderedInnerIndex, renderedOffset + renderedInnerIndex, false);
+                drawItem(innerIndex, offset + innerIndex, true);
             }
+
+            renderedInnerIndex = innerIndex;
+            renderedOffset = offset;
+            hasRendered = true;
+            fullRedraw = false;
             changed = false;
         }
 
@@ -63,6 +67,7 @@ class ListMenu {
             } else if(offset + innerIndex < itemCount-1){
                 offset++;
                 changed = true;
+                fullRedraw = true;
             }
         }
 
@@ -73,6 +78,7 @@ class ListMenu {
             } else if(offset > 0){
                 offset--;
                 changed = true;
+                fullRedraw = true;
             }
         }
 
@@ -80,12 +86,14 @@ class ListMenu {
             int selectedIndex = offset + innerIndex;
             if(selectedIndex < itemCount && items[selectedIndex]->action != nullptr){
                 items[selectedIndex]->action(items[selectedIndex]->params);
+                invalidate();
             }
         }
         bool addtoList(const String& name, void (*action)(void *), void* params = nullptr){
             if(itemCount < 64){
                 items[itemCount] = new ListMenuItem(name, action, params);
                 itemCount++;
+                invalidate();
                 return true;
             }
             return false;
@@ -103,11 +111,8 @@ class ListMenu {
 
             Serial.printf("reached line:%d\n", __LINE__);
             this->textHeight = 8 * textSize+4; // Assuming a base height of 8 pixels for text size 1
-            this->headerHeight = header.length() > 0 ? textHeight : 0;
-            this->maxVisibleItems = (height - headerHeight) / textHeight;
-            if(header.length() > 0 && maxVisibleItems > 0){
-                maxVisibleItems--;
-            }
+            updateLayout();
+            invalidate();
             
         }
     
@@ -127,4 +132,48 @@ class ListMenu {
         uint16_t textHeight;
         String header;
         uint16_t headerHeight;
+        bool fullRedraw;
+        int renderedInnerIndex;
+        int renderedOffset;
+        bool hasRendered;
+
+        void updateLayout(){
+            headerHeight = header.length() > 0 ? textHeight : 0;
+            maxVisibleItems = textHeight > 0
+                ? (height - headerHeight) / textHeight
+                : 0;
+        }
+
+        void clearItems(){
+            System::getInstance().gfx->fillRect(
+                x,
+                y + headerHeight,
+                width,
+                height - headerHeight,
+                COLOR_BACKGROUND);
+        }
+
+        void drawItem(int visibleIndex, int itemIndex, bool selected){
+            if(visibleIndex < 0 || visibleIndex >= maxVisibleItems){
+                return;
+            }
+
+            const int16_t itemY = y + headerHeight + visibleIndex * textHeight;
+            System::getInstance().gfx->fillRect(
+                x + 2,
+                itemY + 2,
+                width - 4,
+                textHeight,
+                selected ? RGB565_CYAN : COLOR_BACKGROUND);
+
+            if(itemIndex < 0 || itemIndex >= itemCount || itemIndex >= 64 ||
+               items[itemIndex]->name.length() == 0){
+                return;
+            }
+
+            System::getInstance().gfx->setTextColor(
+                selected ? RGB565_BLACK : RGB565_WHITE);
+            System::getInstance().gfx->setCursor(x + 5, itemY + 5);
+            System::getInstance().gfx->print(items[itemIndex]->name);
+        }
     };
